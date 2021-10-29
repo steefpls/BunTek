67b25bf26ee33ee31bb6cfff918483c0da35a2a6
Bryan Boh <Bryanboh@gmail.com>
2021-10-29 12:59:05 +0800

    Created Screen switching and button click detection
    Created Screen Struct, and Button Object Struct.
    Created enum for screen names and button effects.
    Created function for screen switching and pause game

---
+++ BunTek/Screen_control.h
@@ -0,0 +1,100 @@
+#pragma once
+#include "SteefLibrary.h"
+#include <string.h>
+
+#define Screen struct screen
+
+#define ButtonObject struct buttonObject
+
+#define LineArrayLength 1000
+#define CircleGameObjectArrayLength 1000
+#define BoxGameObjectArrayLength 1000
+#define ButtonObjectArrayLength 50
+#define TextLimit 100
+
+//names of the screen
+typedef enum{
+	Test_Menu,
+	Test_Room,
+	Splash_screen,
+	Main_menu,
+	//V make sure this is last in the ENUM
+	Total_screen_number,
+} Screen_name;
+
+typedef enum {
+	Move_to_test_room,
+	Move_to_test_Menu,
+	Pause_Game,
+} Button_effects;
+
+struct buttonObject {
+	BoxGameObject boxGameObject;
+	Button_effects button_effect;
+	char buttontext[TextLimit];
+};
+
+struct screen {
+	//Array for all collidable circles
+	int CircleArrayLengthCounter;
+	CircleGameObject CircleGameObjectArray[CircleGameObjectArrayLength];
+	//Array for all collidable Line
+	int LineArrayLengthCounter;
+	BoxGameObject LineArray[LineArrayLength];
+	//Array for all collidable rects
+	int BoxGameObjectArrayLengthCounter;
+	BoxGameObject BoxGameObjectArray[BoxGameObjectArrayLength];
+	//Array for all buttons
+	int ButtonObjectArrayLengthCounter;
+	ButtonObject ButtonObjectArray[ButtonObjectArrayLength];
+};
+//create button object for screen
+ButtonObject CreateButtonObject(Vector2 position, float width, float height,float bounciness, float angle, CP_Color color, Button_effects effect, char* buttontext) {
+	ButtonObject b;
+	b.boxGameObject = CreateBoxGameObject(position, width, height, bounciness, angle);
+	b.boxGameObject.gameObject.color = color;
+	b.button_effect = effect;
+	strcpy_s(b.buttontext, TextLimit, buttontext);
+	return b;
+}
+
+//transition to black
+bool screen_transition_to_black(int* transition_oppacity) {
+	//fade to black
+	*transition_oppacity = CP_Math_LerpInt(*transition_oppacity, 255, FrameTime*10);
+	CP_Math_ClampInt(*transition_oppacity, 0, 255);
+	CP_Settings_Fill(CP_Color_Create(0, 0, 0, *transition_oppacity));
+	CP_Graphics_DrawRectAdvanced(0, 0, (float)CP_System_GetWindowWidth(), (float)CP_System_GetWindowHeight(), 0, 0);
+	if (*transition_oppacity >= 230) {
+		return true;
+	}
+	return false;
+}
+//transition from black
+bool screen_transition_from_black(int* transition_oppacity) {
+	//fade from black
+	*transition_oppacity = CP_Math_LerpInt(*transition_oppacity, 0, FrameTime);
+	CP_Math_ClampInt(*transition_oppacity, 0, 255);
+	CP_Settings_Fill(CP_Color_Create(0, 0, 0, *transition_oppacity));
+	CP_Graphics_DrawRectAdvanced(0, 0, (float)CP_System_GetWindowWidth(), (float)CP_System_GetWindowHeight(), 0, 0);
+	if (*transition_oppacity <= 5) {
+		return true;
+	}
+	return false;
+}
+//transition Control
+void screen_transition(bool* isScreenTransiting, int* transition_oppacity, Screen_name* Current_screen_name, Screen_name* Next_screen_name, Screen* current_screen, const Screen* screen_array) {
+	if (*isScreenTransiting) {//transitioning to new screen
+		if (*Current_screen_name != *Next_screen_name) {
+			if (screen_transition_to_black(transition_oppacity)) {
+				*Current_screen_name = *Next_screen_name;
+				*current_screen = screen_array[*Current_screen_name];
+			}
+		}
+		else {
+			*isScreenTransiting = !screen_transition_from_black(transition_oppacity);
+		}
+	}
+}
+
+
--- BunTek/game.c
+++ BunTek/game.c
@@ -11,8 +11,7 @@
 *
  * Copyright © 2020 DigiPen, All rights reserved.
 * ---------------------------------------------------------*/
-#include "SteefLibrary.h"
-
+#include "Screen_control.h"
 //Initialize variables
 bool Mouse1Held = false; //mouse held bool for previous frame
 Vector2 MousePosPrev;
@@ -26,59 +25,69 @@ Vector2 LineStartPos;
 Vector2 LineEndPos;
 const float DrawnLineBounciness = 1.0f;
 
+bool ButtonClicked = false;
+
 //Initialize functions
 void draw_framerate(void);
 void DrawAllShapes(void);
 void CalculateAllPhysics(void);
 void AddLine(void);
+bool CheckAllButtons(void);
+void TriggerButtonEffects(ButtonObject* x);
+void Initialize_Screens(void);
 
-//Initialize arrays
+//Initialize array
+Screen screen_array[Total_screen_number];
 
-//Array for all collidable circles
-int CircleArrayLengthCounter = 0;
-#define CircleGameObjectArrayLength 1000
-CircleGameObject CircleGameObjectArray[CircleGameObjectArrayLength];
+//Initialize Screen control
+Screen_name Start_Screen = Test_Menu;
+Screen_name Current_screen_name;
+Screen_name Next_screen_name;
 
-//Array for all collidable rects
-int LineArrayLengthCounter = 0;
-#define LineArrayLength 1000
-BoxGameObject LineArray[LineArrayLength];
+//struct screen being displayed and calculated
+Screen Current_screen;
+Screen* current_screen = &Current_screen;
 
+//transition control
+int transition_oppacity = 0;
+bool isScreenTransiting = false;
+
+bool isgamepaused = false;
 
 
 
 void game_init(void)
 {
+    CP_Font_Set(CP_Font_GetDefault());
     if (debug) {
         CP_System_ShowConsole();
     }
-    
-    //Fill up array with test circles
-    for (int i = 0; i < 200; i++) {
-        float scale = CP_Random_RangeFloat(10, 15);
-        CircleGameObject tempc = CreateCircleGameObject(newVector2(CP_Random_RangeFloat(50, 1800), CP_Random_RangeFloat(50, 1000)), newVector2(CP_Random_RangeFloat(-100, 100), CP_Random_RangeFloat(-100, 100)), 0, CP_Color_Create(CP_Random_RangeInt(0, 255), CP_Random_RangeInt(0, 255), CP_Random_RangeInt(0, 255), 255), scale, false, scale,1);
-        CircleGameObjectArray[i] = tempc;
-    }
-
-    
-
+    //initialize all screen data
+    Initialize_Screens();
+    //set the screens
+    Current_screen_name = Start_Screen;
+    Next_screen_name = Start_Screen;
+    //initialize current screen
+    *current_screen = screen_array[Current_screen_name];
 }
 
 void game_update(void)
 {
     CP_Graphics_ClearBackground(CP_Color_Create(50, 50, 50, 255));
     MousePos = newVector2(CP_Input_GetMouseX(), CP_Input_GetMouseY());
-    AddLine();
-    
+    if (!isScreenTransiting) {//not transitioning to new screen
+        //check for button Click and return bool, also run the buttons effect if any
+        ButtonClicked = CheckAllButtons();
+        if (!isgamepaused) {
+            if (!ButtonClicked) {
+                AddLine();
+            }
+            CalculateAllPhysics();
+        }
+    }
     DrawAllShapes();
-    CalculateAllPhysics();
-
-    printf("Mouse 1 held: %d\n",Mouse1Held);
-
-
-    
-    
-        
+    screen_transition(&isScreenTransiting, &transition_oppacity, &Current_screen_name, &Next_screen_name, current_screen, screen_array);
+    printf("Mouse 1 held: %d\n", Mouse1Held);
     // Profiling info and frameRate testing
     if (debug) draw_framerate();
 }
@@ -110,7 +119,7 @@ void AddLine(void) {
             LineLength = DistBetween(LineStartPos, LineEndPos);
         }
         if (MousePos.x != MousePosPrev.x || MousePos.y != MousePosPrev.y) {
-            LineArray[LineCounter] = CreateBoxGameObject(LineStartPos, LineLength, 4.0f, DrawnLineBounciness,atan2f(LineEndPos.y - LineStartPos.y, LineEndPos.x - LineStartPos.x) / (float)PI * 180);
+            Current_screen.LineArray[LineCounter] = CreateBoxGameObject(LineStartPos, LineLength, 4.0f, DrawnLineBounciness,atan2f(LineEndPos.y - LineStartPos.y, LineEndPos.x - LineStartPos.x) / (float)PI * 180);
         }
     }
     else if (Mouse1Held) 
@@ -129,7 +138,7 @@ void DrawAllShapes(void)
 {
     for (int i = 0; i < CircleGameObjectArrayLength; i++)
     {
-        CircleGameObject* x = &CircleGameObjectArray[i];
+        CircleGameObject* x = &Current_screen.CircleGameObjectArray[i];
 
        
         if ( x->radius == 0.0f) // If radius of thing is 0
@@ -156,7 +165,7 @@ void DrawAllShapes(void)
 
     for (int i = 0; i < LineArrayLength; i++)
     {
-        BoxGameObject* x = &LineArray[i];
+        BoxGameObject* x = &Current_screen.LineArray[i];
 
         if (x->width == 0 || x->height == 0)  // Box has no width or height
         {
@@ -169,6 +178,23 @@ void DrawAllShapes(void)
 
         }
     }
+    for (int i = 0; i < ButtonObjectArrayLength; i++)
+    {
+        ButtonObject* x = &Current_screen.ButtonObjectArray[i];
+
+        if (x->boxGameObject.width == 0 || x->boxGameObject.height == 0)  // Box has no width or height
+        {
+
+        }
+        else {
+            CP_Settings_Fill(x->boxGameObject.gameObject.color);
+            CP_Settings_NoStroke();
+            CP_Graphics_DrawRectAdvanced(x->boxGameObject.gameObject.position.x, x->boxGameObject.gameObject.position.y, x->boxGameObject.width, x->boxGameObject.height, x->boxGameObject.gameObject.angle, 1);
+            CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
+            CP_Settings_TextAlignment(CP_TEXT_ALIGN_H_CENTER, CP_TEXT_ALIGN_V_TOP);
+            CP_Font_DrawTextBox(x->buttontext, x->boxGameObject.gameObject.position.x, x->boxGameObject.gameObject.position.y, x->boxGameObject.width);
+        }
+    }
 }
 
 void CalculateAllPhysics(void)
@@ -177,19 +203,19 @@ void CalculateAllPhysics(void)
     //Calculating circle physics
     for (int i = 0; i < CircleGameObjectArrayLength; i++)
     {
-        if (CircleGameObjectArray[i].radius == 0.0f) // If radius of thing is 0
+        if (Current_screen.CircleGameObjectArray[i].radius == 0.0f) // If radius of thing is 0
         {
             break;
         }
                 
-        CirclePhys(&CircleGameObjectArray[i]);
+        CirclePhys(&Current_screen.CircleGameObjectArray[i]);
     }
 
     //Checking circle-circle collision
     for (int i = 0; i < CircleGameObjectArrayLength; i++)
     {
-        CircleGameObject* c1 = &CircleGameObjectArray[i];
-        if (CircleGameObjectArray[i].radius == 0.0f) // TODO: SUB WITH CODE THAT DETECTS END OF ARRAY
+        CircleGameObject* c1 = &Current_screen.CircleGameObjectArray[i];
+        if (Current_screen.CircleGameObjectArray[i].radius == 0.0f) // TODO: SUB WITH CODE THAT DETECTS END OF ARRAY
         {
             //End of Array
             break;
@@ -198,8 +224,8 @@ void CalculateAllPhysics(void)
         {
             for (int o = i+1; o < CircleGameObjectArrayLength; o++)
             {
-                CircleGameObject* c2 = &CircleGameObjectArray[o];
-                if (CircleGameObjectArray[o].radius == 0.0f) // TODO: SUB WITH CODE THAT DETECTS END OF ARRAY
+                CircleGameObject* c2 = &Current_screen.CircleGameObjectArray[o];
+                if (Current_screen.CircleGameObjectArray[o].radius == 0.0f) // TODO: SUB WITH CODE THAT DETECTS END OF ARRAY
                 {
                     //End of Array
                     break;
@@ -226,15 +252,15 @@ void CalculateAllPhysics(void)
 
     //Checking box-circle collision
     for (int x = 0; x < LineArrayLength; x++) {
-        BoxGameObject* b1 = &LineArray[x];
-        if (LineArray[x].height == 0.0f || LineArray[x].width == 0.0f) {
+        BoxGameObject* b1 = &Current_screen.LineArray[x];
+        if (Current_screen.LineArray[x].height == 0.0f || Current_screen.LineArray[x].width == 0.0f) {
             
         }
         else {
             for (int y = 0; y < CircleGameObjectArrayLength; y++)
             {
-                CircleGameObject* c1 = &CircleGameObjectArray[y];
-                if (CircleGameObjectArray[y].radius == 0.0f) // If radius of thing is 0
+                CircleGameObject* c1 = &Current_screen.CircleGameObjectArray[y];
+                if (Current_screen.CircleGameObjectArray[y].radius == 0.0f) // If radius of thing is 0
                 {
                     break;
                 }
@@ -250,7 +276,61 @@ void CalculateAllPhysics(void)
     
 }
 
+bool CheckAllButtons(void) {
+    if (CP_Input_MouseTriggered(MOUSE_BUTTON_LEFT)) {
 
+        for (int i = 0; i < ButtonObjectArrayLength; i++)
+        {
+            ButtonObject* x = &Current_screen.ButtonObjectArray[i];
+            //if mouse is coliding with button
+            if (PointRectCol(MousePos, &x->boxGameObject)) {
+                TriggerButtonEffects(x);
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+void TriggerButtonEffects(ButtonObject* x) {
+    //check what is the buttons effect and run accordingly
+    switch (x->button_effect)
+    {
+    case Move_to_test_room:
+        isgamepaused = false;
+        Next_screen_name = Test_Room;
+        isScreenTransiting = true;
+        break;
+    case Move_to_test_Menu:
+        isgamepaused = false;
+        Next_screen_name = Test_Menu;
+        isScreenTransiting = true;
+        break;
+    case Pause_Game:
+        isgamepaused = !isgamepaused;
+        break;
+    default:
+        break;
+    }
+}
+
+void Initialize_Screens(void) {
+    //Create Test menu screen
+    screen_array[Test_Menu].ButtonObjectArrayLengthCounter = 0;
+    screen_array[Test_Menu].ButtonObjectArray[0] = CreateButtonObject(newVector2(10, 10), 100, 100, 0, 0, CP_Color_Create(255, 255, 255, 200), Move_to_test_room, "Move to Test Screen");
+
+    //Create Test Room screen
+    //Fill up array with test circles
+    for (int i = 0; i < 200; i++) {
+        float scale = CP_Random_RangeFloat(10, 15);
+        CircleGameObject tempc = CreateCircleGameObject(newVector2(CP_Random_RangeFloat(50, 1800), CP_Random_RangeFloat(50, 1000)), newVector2(CP_Random_RangeFloat(-100, 100), CP_Random_RangeFloat(-100, 100)), 0, CP_Color_Create(CP_Random_RangeInt(0, 255), CP_Random_RangeInt(0, 255), CP_Random_RangeInt(0, 255), 255), scale, false, scale, 1);
+        screen_array[Test_Room].CircleGameObjectArray[i] = tempc;
+    }
+    screen_array[Test_Room].LineArrayLengthCounter = 0;
+    screen_array[Test_Room].CircleArrayLengthCounter = 0;
+    screen_array[Test_Room].ButtonObjectArray[0] = CreateButtonObject(newVector2(10, 10), 100, 100, 0, 0, CP_Color_Create(255, 255, 255, 200), Move_to_test_Menu, "Move to Test Menu");
+    screen_array[Test_Room].ButtonObjectArray[1] = CreateButtonObject(newVector2(120, 10), 100, 100, 0, 0, CP_Color_Create(255, 255, 255, 200), Pause_Game, "Pause Game");
+}
 void game_exit(void)
 {
 
